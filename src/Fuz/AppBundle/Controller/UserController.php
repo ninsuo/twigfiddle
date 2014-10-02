f0d9abdd0e19b08b84ace95f1035b0d649192877
alain <ninsuo@gmail.com>
2014-10-02 22:22:32 +0200

    sensioconnect login (with hwi patches in doc)

---
+++ src/Fuz/AppBundle/Controller/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Fuz\AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class UserController extends Controller
+{
+
+    public function indexAction()
+    {
+        return array ();
+    }
+
+}
