e97ef2fd5e9e4f69943394491929fbcffff99ddd
alain <ninsuo@gmail.com>
2014-10-19 20:54:01 +0200

    removed comments

--- src/Fuz/AppBundle/DependencyInjection/FuzAppExtension.php
+++ src/Fuz/AppBundle/DependencyInjection/FuzAppExtension.php
@@ -21,8 +21,5 @@ class FuzAppExtension extends Extension
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
-
-//        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-//        $loader->load('services.yml');
     }
 }
