6d8bdb4666b5d137d68df6d8497722b88d974ad1
alain <ninsuo@gmail.com>
2014-10-19 19:07:01 +0200

    added daemon-runner service

---
+++ src/Fuz/AppBundle/Component/Daemon.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Fuz\AppBundle\Component;
+
+use Symfony\Component\HttpKernel\KernelInterface;
+
+/**
+ * This class intends to run a Linux command as daemon.
+ */
+class Daemon
+{
+
+    const PHPCLI_DIR = '/usr/bin/php';
+
+    protected $kernel;
+
+    public function __construct(KernelInterface $kernel)
+    {
+        $this->kernel = $kernel;
+    }
+
+    public function exec($command, $arguments = array ())
+    {
+        foreach ($arguments as $argument)
+        {
+            $command .= ' ' . escapeshellarg($argument);
+        }
+        if (php_sapi_name() !== "cli")
+        {
+            $this->cliize($command);
+        }
+        else
+        {
+            $this->daemonize($command);
+        }
+    }
+
+    public function bind($command, $parameters = array ())
+    {
+        foreach ($parameters as $placeholder => $value)
+        {
+            $command = str_replace(":{$placeholder}", escapeshellarg($value));
+        }
+        return $command;
+    }
+
+    protected function cliize($command)
+    {
+        $console = escapeshellarg($this->kernel->getRootDir() . '/console');
+        $escaped_command = escapeshellarg($command);
+        exec(self::PHPCLI_DIR . " $console exec:daemon {$escaped_command} >> /dev/null 2>&1 &");
+    }
+
+    protected function daemonize($command)
+    {
+        $parent_pid = pcntl_fork();
+        if ($parent_pid == -1)
+        {
+            throw new \Exception("Failed to fork() !");
+        }
+        if ($parent_pid)
+        {
+            return;
+        }
+        else
+        {
+            posix_setsid();
+            exec("{$command} >> /dev/null 2>&1 &");
+        }
+    }
+
+}
