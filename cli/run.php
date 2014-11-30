e4486acd88da9c3019a41151e12cc2132e6f413c
alain <ninsuo@gmail.com>
2014-11-30 17:54:28 +0100

    clean

--- cli/run.php
+++ cli/run.php
@@ -6,7 +6,6 @@ if (!include __DIR__ . '/vendor/autoload.php')
     die('You must set up the project dependencies.');
 }
 
-use Fuz\Framework\Application;
-
-$app = new Application();
+$app = new Fuz\Framework\Application();
 $app->run();
+
