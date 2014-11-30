d2393d102e9f4dd278694117c08b3f29d77e7b16
alain <ninsuo@gmail.com>
2014-11-30 14:46:08 +0100

    renamed shared => environment

--- runner/demo/demo.php
+++ runner/demo/demo.php
@@ -43,6 +43,6 @@ $context->setFormat(FiddleContext::FORMAT_YAML);
 $context->setContent(file_get_contents('context.yml'));
 $fiddle->setContext($context);
 
-$storage = new StorageFile(__DIR__ . '/../../shared/demo.fiddle');
+$storage = new StorageFile(__DIR__ . '/../../environment/demo.fiddle');
 $shared = new SharedMemory($storage);
 $shared->fiddle = $fiddle;
