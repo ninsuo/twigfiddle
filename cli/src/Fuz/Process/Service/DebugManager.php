9af820ba9d32be827d5efc40e5b9640ffdb8b5c3
alain <ninsuo@gmail.com>
2014-12-06 11:15:15 +0100

    put serialized context file's name in config file

--- cli/src/Fuz/Process/Service/DebugManager.php
+++ cli/src/Fuz/Process/Service/DebugManager.php
@@ -59,7 +59,7 @@ class DebugManager extends BaseService
             $target = $this->debugConfiguration['directory'] . DIRECTORY_SEPARATOR . $environmentId;
             $this->logger->debug("Copying {$source} to {$target}");
             $this->fileSystem->copyDirectory($source, $target);
-            file_put_contents($target . DIRECTORY_SEPARATOR . "context.srz", serialize($context));
+            file_put_contents($target . DIRECTORY_SEPARATOR . $this->debugConfiguration['context_file'], serialize($context));
         }
     }
 
