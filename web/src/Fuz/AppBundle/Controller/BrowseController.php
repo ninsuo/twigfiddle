3121b315fc49a0be306558d676e8d2da03dcca70
alain <ninsuo@gmail.com>
2014-12-30 14:58:48 +0100

    clickable tags

--- web/src/Fuz/AppBundle/Controller/BrowseController.php
+++ web/src/Fuz/AppBundle/Controller/BrowseController.php
@@ -3,13 +3,29 @@
 namespace Fuz\AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Fuz\AppBundle\Base\BaseController;
 
 class BrowseController extends BaseController
 {
 
-    public function indexAction()
+   /**
+     * Loads fiddle's browser
+     *
+     * @Route(
+     *      "/{tag}",
+     *      name = "browse",
+     *      defaults = {
+     *          "tag" = null,
+     *      }
+     * )
+     * @Method({"GET"})
+     * @param string|null $tag
+     * @Template()
+     * @return array
+     */
+    public function indexAction($tag)
     {
         return array ();
     }
--- web/src/Fuz/AppBundle/Resources/public/js/jquery.symfony2.js
+++ web/src/Fuz/AppBundle/Resources/public/js/jquery.symfony2.js
@@ -2,80 +2,92 @@
  * jQuery Symfony2 utilities
  */
 ;
-$.symfony2 = (function ($) {
+(function( $ ) {
 
-    var _entityMap = {
-        "&": "&amp;",
-        "<": "&lt;",
-        ">": "&gt;",
-        '"': '&quot;',
-        "'": '&#39;',
-        "/": '&#x2F;'
-    };
+    $.symfony2 = (function ($) {
 
-    var _escapeHtml = function(string) {
-        return String(string).replace(/[&<>"'\/]/g, function (s) {
-            return _entityMap[s];
-        });
-    };
+        var _entityMap = {
+            "&": "&amp;",
+            "<": "&lt;",
+            ">": "&gt;",
+            '"': '&quot;',
+            "'": '&#39;',
+            "/": '&#x2F;'
+        };
 
-    /*
-     * Dispatch form validation errors at their right location when form was
-     * submit using ajax.
-     *
-     * @param object errors
-     *      Object containing fields id as key, and an array of errors as error messages
-     * @param string generalSelector
-     *      Selector that will contain general errors
-     * @param string specificSelectorPrefix
-     *      Prefix for field-specific error containers
-     * @param string allErrorsSelector
-     *      Selector used by all error containers, so they can be cleaned if error was fixed
-     * @see Fuz\AppBundle\BaseController::getErrorMessagesAjaxFormat
-     */
-    var dispatchErrors = function (errors, generalSelector, specificSelectorPrefix, allErrorsSelector) {
-
-        if (undefined === allErrorsSelector) {
-            allErrorsSelector = '.error-container';
-        }
-
-        $(allErrorsSelector).html('');
-
-        if (undefined === errors) {
-            return;
-        }
-
-        if (undefined === generalSelector) {
-            generalSelector = '#general-errors';
-        }
-        if (undefined === specificSelectorPrefix) {
-            specificSelectorPrefix = '#errors-';
-        }
-
-        if (($(generalSelector).length > 0) && (undefined !== errors['#'])) {
-            var html = '<div class="row" id="errors"><div class="col-md-12 alert alert-danger text-center">';
-            $.each(errors['#'], function (index, error) {
-                html += '<div class="error">' + _escapeHtml(error) + '</div>';
+        /*
+         * Escapes HTML entities
+         *
+         * @param string string
+         *      String to escape
+         * @returns string
+         *      Escaped string
+         */
+        var escape = function(string) {
+            return String(string).replace(/[&<>"'\/]/g, function (s) {
+                return _entityMap[s];
             });
-            html += '</div><div>&nbsp;</div></div>';
-            $(generalSelector).html(html);
-        }
+        };
 
-        $.each(errors, function (id, fieldErrors) {
-            if (('#' !== id) && ($(specificSelectorPrefix + id).length > 0)) {
-                var html = '';
-                $.each(fieldErrors, function (key, value) {
-                    html += '<div class="control-label text-danger">' + _escapeHtml(value) + '</div>';
-                });
-                $(specificSelectorPrefix + id).html(html);
+        /*
+         * Dispatch form validation errors at their right location when form was
+         * submit using ajax.
+         *
+         * @param object errors
+         *      Object containing fields id as key, and an array of errors as error messages
+         * @param string generalSelector
+         *      Selector that will contain general errors
+         * @param string specificSelectorPrefix
+         *      Prefix for field-specific error containers
+         * @param string allErrorsSelector
+         *      Selector used by all error containers, so they can be cleaned if error was fixed
+         * @see Fuz\AppBundle\BaseController::getErrorMessagesAjaxFormat
+         */
+        var dispatchErrors = function (errors, generalSelector, specificSelectorPrefix, allErrorsSelector) {
+
+            if (undefined === allErrorsSelector) {
+                allErrorsSelector = '.error-container';
             }
-        });
 
-    }; // dispatchErrors
+            $(allErrorsSelector).html('');
 
-    return {
-        dispatchErrors: dispatchErrors
-    };
+            if (undefined === errors) {
+                return;
+            }
 
-})(jQuery); // $.symfony2
+            if (undefined === generalSelector) {
+                generalSelector = '#general-errors';
+            }
+            if (undefined === specificSelectorPrefix) {
+                specificSelectorPrefix = '#errors-';
+            }
 
+            if (($(generalSelector).length > 0) && (undefined !== errors['#'])) {
+                var html = '<div class="row" id="errors"><div class="col-md-12 alert alert-danger text-center">';
+                $.each(errors['#'], function (index, error) {
+                    html += '<div class="error">' + escape(error) + '</div>';
+                });
+                html += '</div><div>&nbsp;</div></div>';
+                $(generalSelector).html(html);
+            }
+
+            $.each(errors, function (id, fieldErrors) {
+                if (('#' !== id) && ($(specificSelectorPrefix + id).length > 0)) {
+                    var html = '';
+                    $.each(fieldErrors, function (key, value) {
+                        html += '<div class="control-label text-danger">' + escape(value) + '</div>';
+                    });
+                    $(specificSelectorPrefix + id).html(html);
+                }
+            });
+
+        }; // dispatchErrors
+
+        return {
+            escape: escape,
+            dispatchErrors: dispatchErrors
+        };
+
+    })( $ ); // $.symfony2
+
+})( jQuery );
