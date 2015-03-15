39e5a5444180e39726e1a08b497b6f01d02352bd
alain <ninsuo@gmail.com>
2015-03-15 13:12:02 +0100

    small fixes

--- web/src/Fuz/AppBundle/Command/ImportCommand.php
+++ web/src/Fuz/AppBundle/Command/ImportCommand.php
@@ -87,7 +87,6 @@ class ImportCommand extends ContainerAwareCommand
                 if ($fiddle->getId())
                 {
                     $em->remove($template);
-                    $em->flush();
                 }
             }
 
@@ -109,7 +108,6 @@ class ImportCommand extends ContainerAwareCommand
                 if ($fiddle->getId())
                 {
                     $em->remove($tag);
-                    $em->flush();
                 }
             }
 
