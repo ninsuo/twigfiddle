6b65979cd020ee2a119699845c29f49d24ad7d03
alain tiemblo <ninsuo@gmail.com>
2015-08-13 19:34:07 +0200

    optimized preparation script

--- cli/twig/prepare.sh
+++ cli/twig/prepare.sh
@@ -12,12 +12,15 @@ php security.php
 # Compilling all C extensions
 for c_ext in uncompressed/*/ext/twig;
 do
-    echo "compile ${c_ext}...";
-    (
-        cd $c_ext;
-        phpize;
-        ./configure;
-        make
-    )
+    if ! [ -f "$c_ext/modules/twig.so" ]
+    then
+        echo "compile ${c_ext}...";
+        (
+            cd $c_ext;
+            phpize;
+            ./configure;
+            make
+        )
+    fi
 done
 
