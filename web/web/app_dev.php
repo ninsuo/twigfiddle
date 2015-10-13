349b0d2f97e262a8c85b6078e1e4d90d351d007e
alain tiemblo <ninsuo@gmail.com>
2015-10-13 22:45:51 +0200

    changed ip

--- web/web/app_dev.php
+++ web/web/app_dev.php
@@ -11,7 +11,7 @@ use Symfony\Component\Debug\Debug;
 // Feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-     || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1', '80.248.218.225'))
+     || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1', '62.210.125.118'))
      || php_sapi_name() === 'cli-server')
 ) {
     header('HTTP/1.0 403 Forbidden');
