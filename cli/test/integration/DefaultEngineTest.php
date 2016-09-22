31d4a2d2146c437dfd113e788311e1e1e8c9b764
alain tiemblo <ninsuo@gmail.com>
2016-09-22 07:36:04 +0200

    Added Twig 1.25.0 support

--- cli/test/integration/DefaultEngineTest.php
+++ cli/test/integration/DefaultEngineTest.php
@@ -71,6 +71,7 @@ class DefaultEngineTest extends \PHPUnit_Framework_TestCase
             array('Twig 2.x', 'Twig-master'),
 
             // 1.x
+            array('Twig 1.x', 'Twig-1.25.0'),
             array('Twig 1.x', 'Twig-1.24.2'),
             array('Twig 1.x', 'Twig-1.24.1'),
             array('Twig 1.x', 'Twig-1.24.0'),
