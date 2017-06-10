883666f781084ab5d444c091f65611397b615981
alain tiemblo <ninsuo@gmail.com>
2017-06-10 16:06:20 +0200

    automate new release installations and configuration

---
+++ cli/src/Fuz/Process/Command/ReleaseWatcherCommand.php
@@ -0,0 +1,202 @@
+<?php
+
+/*
+ * This file is part of twigfiddle.com project.
+ *
+ * (c) Alain Tiemblo <alain@fuz.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Fuz\Process\Command;
+
+use Fuz\Framework\Base\BaseCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Yaml\Yaml;
+use GuzzleHttp\Client;
+use Ramsey\Uuid\Uuid;
+
+class ReleaseWatcherCommand extends BaseCommand
+{
+    protected $cliDir;
+
+    public function __construct($name = null)
+    {
+        parent::__construct($name);
+
+        $this->cliDir = __DIR__ . '/../../../../../cli/';
+    }
+
+    protected function configure()
+    {
+        parent::configure();
+
+        $this
+           ->setName('twigfiddle:release:watcher')
+           ->setDescription('Fetch, configure and install new Twig and Twig-extension releases')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this
+           ->fetchNewTwigReleases()
+           ->fetchNewTwigExtensionsReleses()
+        ;
+    }
+
+    protected function fetchNewTwigExtensionsReleses()
+    {
+        $response = (new Client())->get('https://api.github.com/repos/twigphp/twig-extensions/tags', [
+            'headers' => [
+                'Accept' => 'application/vnd.github.mercy-preview+json',
+            ]
+        ]);
+
+        $result = json_decode($response->getBody()->getContents(), true);
+        foreach ($result as $tag) {
+            $version = 'Twig-extensions-' . substr($tag['name'], 1);
+
+            $gzPath = $this->cliDir."/twig/extension/compressed/{$version}.tar.gz";
+            if (is_file($gzPath)) {
+                continue ;
+            }
+
+            $this->downloadAndPrepare($tag['tarball_url'], $version, true);
+        }
+
+        return $this;
+    }
+
+    protected function fetchNewTwigReleases()
+    {
+        $newReleasesCounter = 0;
+
+        $response = (new Client())->get('https://api.github.com/repos/twigphp/twig/tags', [
+            'headers' => [
+                'Accept' => 'application/vnd.github.mercy-preview+json',
+            ]
+        ]);
+
+        $result = json_decode($response->getBody()->getContents(), true);
+        foreach ($result as $tag) {
+            $version = 'Twig-' . substr($tag['name'], 1);
+
+            $gzPath = $this->cliDir."/twig/compressed/{$version}.tar.gz";
+            if (is_file($gzPath)) {
+                continue ;
+            }
+
+            $this->downloadAndPrepare($tag['tarball_url'], $version, false);
+
+            $newReleasesCounter++;
+        }
+
+        if ($newReleasesCounter) {
+            $this->regenerateConfigurationAndTests();
+        }
+
+        return $this;
+    }
+
+    protected function downloadAndPrepare($url, $version, $isExtension)
+    {
+        // --------------------------------------
+        // downloading release
+        // --------------------------------------
+
+        $gz      = (new Client())->get($url)->getBody()->getContents();
+        $tmpDir  = '/tmp/'.Uuid::uuid4();
+        mkdir($tmpDir);
+        file_put_contents("{$tmpDir}/release.tgz", $gz);
+
+        // --------------------------------------
+        // renaming random directory name to Twig-x.y.z
+        // --------------------------------------
+
+        exec("( cd $tmpDir && tar xzf release.tgz )");
+        $twigDir = basename(glob("{$tmpDir}/*Twig-*")[0]);
+        rename("{$tmpDir}/{$twigDir}", "{$tmpDir}/{$version}");
+        exec("( cd $tmpDir && tar czf {$version}.tar.gz {$version} )");
+
+        // --------------------------------------
+        // moving files to the right places
+        // --------------------------------------
+
+        if ($isExtension) {
+            rename("{$tmpDir}/{$version}.tar.gz", $this->cliDir."/twig/extension/compressed/{$version}.tar.gz");
+            rename("{$tmpDir}/{$version}", $this->cliDir."/twig/extension/uncompressed/{$version}");
+        } else {
+            rename("{$tmpDir}/{$version}.tar.gz", $this->cliDir."/twig/compressed/{$version}.tar.gz");
+            rename("{$tmpDir}/{$version}", $this->cliDir."/twig/uncompressed/{$version}");
+        }
+
+        exec("rm -rf $tmpDir"); // hopefully, Uuid::uuid4 won't return "../" :)
+    }
+
+    protected function regenerateConfigurationAndTests()
+    {
+        // --------------------------------------
+        // getting and sorting all available versions
+        // --------------------------------------
+
+        $versions  = [];
+        $twigPaths = glob($this->cliDir.'/twig/compressed/Twig-*.tar.gz');
+        foreach ($twigPaths as $twigPath) {
+            $versions[] = substr(basename($twigPath), 0, strpos(basename($twigPath), '.tar.gz'));
+        }
+
+        usort($versions, 'version_compare');
+        $versionsReversed = array_reverse($versions);
+
+        $dividedInMajorVersions = [
+            '0' => [],
+            '1' => [],
+            '2' => [],
+        ];
+
+        foreach ($versionsReversed as $version) {
+            $dividedInMajorVersions[substr($version, 5, 1)][] = $version;
+        }
+
+        // --------------------------------------
+        // regenerating twig engines configuration
+        // --------------------------------------
+
+        $config = $this->cliDir.'/config/services/twig_engines.yml';
+        $yml = Yaml::parse($config);
+
+        $yml['services']['v2.twig_engine']['tags'][0]['versions'] = implode(' / ', $dividedInMajorVersions['2']);
+        $yml['services']['v1.twig_engine']['tags'][0]['versions'] = implode(' / ', $dividedInMajorVersions['1']);
+        $yml['services']['v0.twig_engine']['tags'][0]['versions'] = implode(' / ', $dividedInMajorVersions['0']);
+
+        file_put_contents($config, Yaml::dump($yml, 5));
+
+        // --------------------------------------
+        // regenerating tests
+        // --------------------------------------
+
+        $test = $this->cliDir.'//test/integration/DefaultEngineTest.php';
+        $content = file_get_contents($test);
+
+        $headerDelimiter = '// --- header for auto-generation ---';
+        $footerDelimiter = '// --- footer for auto-generation ---';
+
+        $header = substr($content, 0, strpos($content, $headerDelimiter) + strlen($headerDelimiter));
+        $footer = substr($content, strpos($content, $footerDelimiter));
+
+        $generated = "\n\n";
+        foreach (array_reverse($dividedInMajorVersions, true) as $majorVersion => $versions) {
+            $generated .= "            // {$majorVersion}.x\n";
+            foreach ($versions as $version) {
+                $generated .= "            array('Twig {$majorVersion}.x', '$version'),\n";
+            }
+            $generated .= "\n";
+        }
+        $generated .= "            ";
+
+        file_put_contents($test, $header.$generated.$footer);
+    }
+}
