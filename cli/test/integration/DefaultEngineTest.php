1a9ac5131d0528c802cc4001a6a98da21d28be3c
ninsuo <ninsuo@gmail.com>
2018-03-03 17:30:10 +0100

    Made new Twig release available at https://twigfiddle.com - enjoy!

--- cli/test/integration/DefaultEngineTest.php
+++ cli/test/integration/DefaultEngineTest.php
@@ -69,6 +69,7 @@ class DefaultEngineTest extends \PHPUnit_Framework_TestCase
             // --- header for auto-generation ---
 
             // 2.x
+            array('Twig 2.x', 'Twig-2.4.6'),
             array('Twig 2.x', 'Twig-2.4.5'),
             array('Twig 2.x', 'Twig-2.4.4'),
             array('Twig 2.x', 'Twig-2.4.3'),
@@ -83,6 +84,7 @@ class DefaultEngineTest extends \PHPUnit_Framework_TestCase
             array('Twig 2.x', 'Twig-2.0.0'),
 
             // 1.x
+            array('Twig 1.x', 'Twig-1.35.2'),
             array('Twig 1.x', 'Twig-1.35.1'),
             array('Twig 1.x', 'Twig-1.35.0'),
             array('Twig 1.x', 'Twig-1.34.4'),
