4e1ea5d3c88b0dfdd2d5e0216fae65f907de1c54
alain tiemblo <ninsuo@gmail.com>
2018-07-03 06:41:25 +0200

    removed typo, fix #45

--- cli/src/Fuz/Process/Service/ErrorManager.php
+++ cli/src/Fuz/Process/Service/ErrorManager.php
@@ -16,31 +16,31 @@ use Fuz\Process\Entity\Error;
 class ErrorManager extends BaseService
 {
     protected $errors = [
-        Error::E_UNKNOWN => [
+        Error::E_UNKNOWN                   => [
             'group'   => Error::G_GENERAL,
             'message' => 'An unknwon error occured.',
             'logger'  => 'error',
             'debug'   => true,
         ],
-        Error::E_UNEXPECTED => [
+        Error::E_UNEXPECTED                => [
             'group'   => Error::G_GENERAL,
             'message' => 'An unexpected error occured.',
             'logger'  => 'error',
             'debug'   => true,
         ],
-        Error::E_UNEXPECTED_NODEBUG => [
+        Error::E_UNEXPECTED_NODEBUG        => [
             'group'   => Error::G_GENERAL,
             'message' => 'An unexpected error occured.',
             'logger'  => 'error',
             'debug'   => false,
         ],
-        Error::E_TIMEOUT => [
+        Error::E_TIMEOUT                   => [
             'group'   => Error::G_GENERAL,
             'message' => 'Maximum execution timeout was reached.',
             'logger'  => 'info',
             'debug'   => false,
         ],
-        Error::E_INVALID_ENVIRONMENT_ID => [
+        Error::E_INVALID_ENVIRONMENT_ID    => [
             'group'   => Error::G_ENVIRONMENT,
             'message' => 'The given environment ID does not match with the expected format.',
             'logger'  => 'warning',
@@ -52,133 +52,133 @@ class ErrorManager extends BaseService
             'logger'  => 'warning',
             'debug'   => false,
         ],
-        Error::E_UNEXISTING_SHARED_MEMORY => [
+        Error::E_UNEXISTING_SHARED_MEMORY  => [
             'group'   => Error::G_ENVIRONMENT,
-            'message' => "The envrionment's shared memory does not exist.",
+            'message' => "The envrionment shared memory does not exist.",
             'logger'  => 'warning',
             'debug'   => false,
         ],
-        Error::E_UNREADABLE_SHARED_MEMORY => [
+        Error::E_UNREADABLE_SHARED_MEMORY  => [
             'group'   => Error::G_ENVIRONMENT,
-            'message' => "The envrionment's shared memory is not readable.",
+            'message' => "The envrionment shared memory is not readable.",
             'logger'  => 'error',
             'debug'   => false,
         ],
-        Error::E_FIDDLE_ALREADY_RUN => [
+        Error::E_FIDDLE_ALREADY_RUN        => [
             'group'   => Error::G_ENVIRONMENT,
             'message' => 'This fiddle has already been started.',
             'logger'  => 'info',
             'debug'   => false,
         ],
-        Error::E_FIDDLE_NOT_STORED => [
+        Error::E_FIDDLE_NOT_STORED         => [
             'group'   => Error::G_ENVIRONMENT,
             'message' => 'Fiddle not found in the shared object.',
             'logger'  => 'error',
             'debug'   => false,
         ],
-        Error::E_UNKNOWN_CONTEXT_FORMAT => [
+        Error::E_UNKNOWN_CONTEXT_FORMAT    => [
             'group'   => Error::G_CONTEXT,
             'message' => 'The given context format is not supported.',
             'logger'  => 'error',
             'debug'   => true,
         ],
-        Error::E_INVALID_CONTEXT_SYNTAX => [
+        Error::E_INVALID_CONTEXT_SYNTAX    => [
             'group'   => Error::G_CONTEXT,
             'message' => 'Unable to convert the given twig context to an array.',
             'logger'  => 'info',
             'debug'   => false,
         ],
-        Error::E_INVALID_CONTEXT_TYPE => [
+        Error::E_INVALID_CONTEXT_TYPE      => [
             'group'   => Error::G_CONTEXT,
             'message' => 'The Twig context should be convertible to an array.',
             'logger'  => 'info',
             'debug'   => false,
         ],
-        Error::E_NO_TEMPLATE => [
+        Error::E_NO_TEMPLATE               => [
             'group'   => Error::G_TEMPLATE,
             'message' => 'This fiddle does not contain any template.',
             'logger'  => 'warning',
             'debug'   => false,
         ],
-        Error::E_NO_MAIN_TEMPLATE => [
+        Error::E_NO_MAIN_TEMPLATE          => [
             'group'   => Error::G_TEMPLATE,
             'message' => 'This fiddle does not have a main template.',
             'logger'  => 'warning',
             'debug'   => false,
         ],
-        Error::E_SEVERAL_MAIN_TEMPLATES => [
+        Error::E_SEVERAL_MAIN_TEMPLATES    => [
             'group'   => Error::G_TEMPLATE,
             'message' => 'This fiddle has several main templates.',
             'logger'  => 'warning',
             'debug'   => false,
         ],
-        Error::E_INVALID_TEMPLATE_NAME => [
+        Error::E_INVALID_TEMPLATE_NAME     => [
             'group'   => Error::G_TEMPLATE,
             'message' => 'Invalid template name.',
             'logger'  => 'warning',
             'debug'   => false,
         ],
-        Error::E_CANNOT_WRITE_TEMPLATE => [
+        Error::E_CANNOT_WRITE_TEMPLATE     => [
             'group'   => Error::G_TEMPLATE,
             'message' => 'Unable to write template.',
             'logger'  => 'error',
             'debug'   => true,
         ],
-        Error::E_ENGINE_NOT_FOUND => [
+        Error::E_ENGINE_NOT_FOUND          => [
             'group'   => Error::G_EXECUTION,
             'message' => 'The Twig engine you requested is not implemented.',
             'logger'  => 'warning',
             'debug'   => false,
         ],
-        Error::E_EXECUTION_FAILURE => [
+        Error::E_EXECUTION_FAILURE         => [
             'group'   => Error::G_EXECUTION,
             'message' => 'The fiddle execution failed.',
             'logger'  => 'info',
             'debug'   => false,
         ],
-        Error::E_TWIG_LOADER_ERROR => [
+        Error::E_TWIG_LOADER_ERROR         => [
             'group'   => Error::G_EXECUTION,
             'message' => 'The Twig Loader thrown an exception.',
             'logger'  => 'info',
             'debug'   => false,
         ],
-        Error::E_TWIG_SYNTAX_ERROR => [
+        Error::E_TWIG_SYNTAX_ERROR         => [
             'group'   => Error::G_EXECUTION,
             'message' => 'The given Twig code contains syntax error(s).',
             'logger'  => 'info',
             'debug'   => false,
         ],
-        Error::E_TWIG_RUNTIME_ERROR => [
+        Error::E_TWIG_RUNTIME_ERROR        => [
             'group'   => Error::G_EXECUTION,
-            'message' => "A Twig's runtime exception has been trown.",
+            'message' => "A Twig runtime exception has been thrown.",
             'logger'  => 'info',
             'debug'   => false,
         ],
-        Error::E_UNKNOWN_COMPILED_FILE => [
+        Error::E_UNKNOWN_COMPILED_FILE     => [
             'group'   => Error::G_EXECUTION,
             'message' => "Can't get template name from a compiled file.",
             'logger'  => 'error',
             'debug'   => true,
         ],
-        Error::E_UNEXPECTED_COMPILED_FILE => [
+        Error::E_UNEXPECTED_COMPILED_FILE  => [
             'group'   => Error::G_EXECUTION,
             'message' => 'Got an unexpected template name.',
             'logger'  => 'error',
             'debug'   => true,
         ],
-        Error::E_C_NOT_SUPPORTED => [
+        Error::E_C_NOT_SUPPORTED           => [
             'group'   => Error::G_OPTION,
             'message' => 'The Twig version you requested does not have a C extension.',
             'logger'  => 'info',
             'debug'   => false,
         ],
-        Error::E_C_UNABLE_TO_DL => [
+        Error::E_C_UNABLE_TO_DL            => [
             'group'   => Error::G_OPTION,
             'message' => 'The C extension exists but has not been loaded successfully.',
             'logger'  => 'error',
             'debug'   => true,
         ],
-        Error::E_UNKNOWN_TWIG_EXTENSION => [
+        Error::E_UNKNOWN_TWIG_EXTENSION    => [
             'group'   => Error::G_OPTION,
             'message' => 'The requested Twig extension version is not supported.',
             'logger'  => 'warning',
