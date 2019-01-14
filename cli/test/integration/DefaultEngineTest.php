4108134dced1701e234c34fa00f090edca300791
Alain Tiemblo <alain@fuz.org>
2019-01-14 16:00:11 +0100

    Made new Twig release available at https://twigfiddle.com - enjoy!

--- cli/test/integration/DefaultEngineTest.php
+++ cli/test/integration/DefaultEngineTest.php
@@ -89,6 +89,7 @@ class DefaultEngineTest extends \PHPUnit_Framework_TestCase
             array('Twig 2.x', 'Twig-2.0.0'),
 
             // 1.x
+            array('Twig 1.x', 'Twig-1.37.1'),
             array('Twig 1.x', 'Twig-1.37.0'),
             array('Twig 1.x', 'Twig-1.36.0'),
             array('Twig 1.x', 'Twig-1.35.4'),
